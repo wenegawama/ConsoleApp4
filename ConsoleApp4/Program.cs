0ecc5fea09febed0c5516a1bc50af628beb9b5c0
Wenega Wama <narcissewatherloy@gmail.com>
2022-05-05 17:03:18 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Piramide
+    {
+        static void Main(string[] args)
+        {
+            string name = "narcisse";
+            int age = 32;
+            Console.WriteLine(name);
+            Console.WriteLine(age);
+        }
+    }
+}
